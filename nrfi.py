4296809663e97d01559c02e3d36719c62edcf9b0
bena618 <98246992+bena618@users.noreply.github.com>
2024-04-06 01:18:34 -0400

    Update nrfi.py

--- nrfi.py
+++ nrfi.py
@@ -85,7 +85,8 @@ if response.status_code == 200:
     #[-1] needed if factor in throwing hand of pitcher, if found with above line
     #    pitchers = [" ".join(elem.text.split()[:-1]) for elem in pitchers]
 
-    links = soup.find_all("a")[:-48]    
+    print(soup.find_all("a"))
+#    links = soup.find_all("a")[:-48]    
 #    links = soup.find_all("a")[:-54]
     links = links[477:]
     # [x:x+23] = link with teams names, away pitcher,away lineup, home pitcher,home team,2 links for tickets then alerts
