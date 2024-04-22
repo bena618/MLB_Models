b65a9414fc0464d1abc0338e3d914a913c0354eb
bena618 <98246992+bena618@users.noreply.github.com>
2024-04-22 10:31:16 -0400

    Update withRotoIdea.py

--- withRotoIdea.py
+++ withRotoIdea.py
@@ -339,7 +339,8 @@ if response.status_code == 200:
 
         half_innings.append([awayTeam] + [awayScore])
         half_innings.append([homeTeam] + [homeScore])
-#        print(half_innings)
+        print("Printing hinfs")
+        print(half_innings)
 #        print(indexForOdds)
 
         if indexForOdds:
@@ -354,7 +355,7 @@ if response.status_code == 200:
             else:
                 YRFIs.append([-1] + [f"{awayTeam} @ {homeTeam}({game_times[index//23]})"] + [homeScore + awayScore])
 
-#        print(NRFIs)
+        print(NRFIs)
 #        print(YRFIs)
         '''
         awayScore = 0
