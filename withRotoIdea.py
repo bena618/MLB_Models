af1fd8202d3f9a7172ed19ea44b210bce0578167
bena618 <98246992+bena618@users.noreply.github.com>
2024-04-29 22:38:24 -0400

    Update withRotoIdea.py

--- withRotoIdea.py
+++ withRotoIdea.py
@@ -89,7 +89,7 @@ if response.status_code == 200:
 
     print(matchuplocs)
     for i in range(len(matchuplocs)):
-        if matchuplocs[i+1] - matchuplocs[i+1] < 20:
+        if i+1 < len(mamatchuplocs) and matchuplocs[i+1] - matchuplocs[i] < 20:
             continue
         index = matchuplocs[i]-matchuplocs[0]
     
