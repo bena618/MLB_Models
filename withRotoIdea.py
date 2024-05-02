1c0e3f2c090d4b8158d738833e3ff62aa06a8b46
bena618 <98246992+bena618@users.noreply.github.com>
2024-05-02 11:02:51 -0400

    Update withRotoIdea.py

--- withRotoIdea.py
+++ withRotoIdea.py
@@ -354,7 +354,7 @@ if response.status_code == 200:
         print(f"Predicted total runs: {homeScore + awayScore}")
 
 #        status_index = int(index //11.5) - offsetTeams
-        status_index = int(i // 2)
+        status_index = int(i * 2)
         
         try:
 #            print(index//11.5,offsetTeams,awayTeam,homeTeam)
