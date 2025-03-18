929365791b702bfb1cf5d4f697c4b2cb3b22be59
bena618 <98246992+bena618@users.noreply.github.com>
2025-03-18 01:23:02 -0400

    Update F5AndNRFI.py

--- F5AndNRFI.py
+++ F5AndNRFI.py
@@ -53,7 +53,7 @@ def get_pitcher_data(name):
                 return {"Name": name,"whip": whip_L30}            
         except:
 #            url = 'https://www.statmuse.com/mlb/ask/' + name.lower().replace(' ', '%20') + '%20stats%20last%2010%20games%20including%20whip'
-             url = 'https://www.statmuse.com/mlb/ask/' + name.lower().replace(' ', '%20') + '-stats-last-10-games-including-whip-log'
+            url = 'https://www.statmuse.com/mlb/ask/' + name.lower().replace(' ', '%20') + '-stats-last-10-games-including-whip-log'
             response = requests.get(url, headers=headers)
             try:
                 tables = pd.read_html(response.text)
