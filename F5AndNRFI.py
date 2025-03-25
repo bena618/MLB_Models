a8993a37b5bcd55e8f445b2722b052530448c690
bena618 <98246992+bena618@users.noreply.github.com>
2025-03-25 16:33:04 -0400

    Update F5AndNRFI.py

--- F5AndNRFI.py
+++ F5AndNRFI.py
@@ -225,7 +225,7 @@ teams = []
 game_times = []
 ids = {}
 
-print(response.statuse_code)
+print(response.status_code)
 if response.status_code == 200:
    soup = BeautifulSoup(response.text, 'html.parser')
    pitchers = soup.find_all('div',class_='lineup__player-highlight-name')
