e5d3a857b362f1fe729ebef0d340f3d0da66c849
bena618 <98246992+bena618@users.noreply.github.com>
2025-03-29 08:49:45 -0400

    Changded statmuse queries because was only looking at games where a hit was recorded for some reason, very likely a cause that YRFIs picked so frequently

--- F5AndNRFI.py
+++ F5AndNRFI.py
@@ -106,7 +106,7 @@ def get_batter_data(name):
     elif name == 'Enrique Hernandez':
         name = 'Kike Hernandez'
 
-    url = 'https://www.statmuse.com/mlb/ask/' + name.lower().replace(' ', '%20') + '%20stats%20between%20' + date_N_days_ago_str(todaysDate,7) + '%20and%20' + yesterdaysDate + '%20stats%20including%20obp%20avg%20and%20slg'
+    url = 'https://www.statmuse.com/mlb/ask/' + name.lower().replace(' ', '%20') + '%20stats%20between%20' + date_N_days_ago_str(todaysDate,7) + '%20and%20' + yesterdaysDate + '%20stats%20including%20obp%20avg%20and%20slg+by+game'
 #    print(url)
     print(f"{name}:{url}")
 
@@ -119,6 +119,7 @@ def get_batter_data(name):
         except:
 #            url = 'https://www.statmuse.com/mlb/ask/' + name.lower().replace(' ', '%20') + '%20stats%20including%20obp%20avg%20and%20slg'
             url = 'https://www.statmuse.com/mlb/ask/' + name.lower().replace(' ', '%20') + '%20+stats+last+10+games+obp%2C+avg%2C+and+slg+by+game'
+            print(f"In except: {url}")
             response = requests.get(url,headers=headers)
             try:
                 tables = pd.read_html(response.text)
@@ -134,7 +135,7 @@ def get_batter_data(name):
         df = tables[0].head(25)
                 
         if df['G'].iloc[0] < 3:
-            url = 'https://www.statmuse.com/mlb/ask?q=' + name.lower().replace(' ', '%20') + '%20stats%20last%2010%20games%20%20obp%20avg%20and%20slg'
+            url = 'https://www.statmuse.com/mlb/ask?q=' + name.lower().replace(' ', '%20') + '-stats-last-10-regular-season-games-stats-including-avg-obp-and-slg'
             print(f"Less than 3 games so: {url}")
             response = requests.get(url,headers=headers)
             if response.status_code == 200:
