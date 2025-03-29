f90fae4b11908f649389a562e0643a2625604ad1
bena618 <98246992+bena618@users.noreply.github.com>
2025-03-29 09:52:37 -0400

    Update F5AndNRFI.py

--- F5AndNRFI.py
+++ F5AndNRFI.py
@@ -106,7 +106,7 @@ def get_batter_data(name):
     elif name == 'Enrique Hernandez':
         name = 'Kike Hernandez'
 
-    url = 'https://www.statmuse.com/mlb/ask/' + name.lower().replace(' ', '%20') + '%20stats%20between%20' + date_N_days_ago_str(todaysDate,7) + '%20and%20' + yesterdaysDate + '%20stats%20including%20obp%20avg%20and%20slg+by+game'
+    url = 'https://www.statmuse.com/mlb/ask/' + name.lower().replace(' ', '%20') + '%20stats%20between%20' + date_N_days_ago_str(todaysDate,7) + '%20and%20' + yesterdaysDate + '%20including%20obp%20avg%20and%20slg%20and%20games'
 #    print(url)
     print(f"{name}:{url}")
 
@@ -118,8 +118,9 @@ def get_batter_data(name):
             tables = pd.read_html(response.text)
         except:
 #            url = 'https://www.statmuse.com/mlb/ask/' + name.lower().replace(' ', '%20') + '%20stats%20including%20obp%20avg%20and%20slg'
-            url = 'https://www.statmuse.com/mlb/ask/' + name.lower().replace(' ', '%20') + '%20+stats+last+10+games+obp%2C+avg%2C+and+slg'
-            print(f"Check cumulative stats last 10 games: {url}")
+            #Not sure if matters but was feeling maybe more go off regular season more variety of pitchers and matches intensity of current regular season games more
+            url = 'https://www.statmuse.com/mlb/ask/' + name.lower().replace(' ', '%20') + '-stats-last-10-regular-season-games-including-obp-avg-and-slg-and-game'
+            print(f"Error reading- Stats last 10 reg season games: {url}")
             response = requests.get(url,headers=headers)
             try:
                 tables = pd.read_html(response.text)
@@ -132,11 +133,11 @@ def get_batter_data(name):
 #                return df
                 return None
 
-        df = tables[0].head(1)
+        df = tables[0].head(10)
                 
-        if df['G'].iloc[0] < 3:
-            url = 'https://www.statmuse.com/mlb/ask?q=' + name.lower().replace(' ', '%20') + '-stats-last-10-regular-season-games-stats-including-avg-obp-and-slg'
-            print(f"Less than 3 games so: {url}")
+        if df['G'].sum() < 3:
+            url = 'https://www.statmuse.com/mlb/ask/' + name.lower().replace(' ', '%20') + '%20+stats+last+10+regular+season+games+obp%2C+avg%2C+and+slg+and+games+by+game'
+            print(f"Less than 3 games in last week so: {url}")
             response = requests.get(url,headers=headers)
             if response.status_code == 200:
                 try:
@@ -251,7 +252,7 @@ teams = []
 game_times = []
 ids = {}
 
-print(response.status_code)
+#print(response.status_code)
 if response.status_code == 200:
    soup = BeautifulSoup(response.text, 'html.parser')
    pitchers = soup.find_all('div',class_='lineup__player-highlight-name')
