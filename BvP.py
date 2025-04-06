d105896ca26aa9d8cfefae6af138a846935e01b3
bena618 <98246992+bena618@users.noreply.github.com>
2025-04-06 12:14:04 -0400

    Update BvP.py

--- BvP.py
+++ BvP.py
@@ -224,13 +224,13 @@ if response.status_code == 200:
 
    teams = soup.find_all('div',class_= 'lineup__abbr')
    teams = [elem.text for elem in teams]
-#   teams = teams[:2]
+   teams = teams[2:]
 #   teams[6:] = teams[8:]
    
    game_times = soup.find_all('div',class_="lineup__time")
    game_times = [elem.text for elem in game_times]
 #   game_times[3:] = game_times[4:]
-#   game_times = game_times[:1]
+   game_times = game_times[1:]
 
    confirmedOrExpected = soup.find_all('li',class_="lineup__status")
    confirmedOrExpected = [elem.text.strip().split()[0][0] for elem in confirmedOrExpected]
