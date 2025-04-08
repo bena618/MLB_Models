e56c2b94c9c2f2708e0be5c941b5927a96d53321
bena618 <98246992+bena618@users.noreply.github.com>
2025-04-08 14:15:46 -0400

    Update F5AndNRFI.py

--- F5AndNRFI.py
+++ F5AndNRFI.py
@@ -603,8 +603,7 @@ allTeamLines = allTeamLines[:len(teams)]
 #[print(elem) for elem in enumerate(allTeamLines)]
 odds = []
 for teamLines in allTeamLines:
-    print(f"Text:*{teamLines.find_next('a', class_='highlight').text}*")
-    odds.append(teamLines.find_next('a', class_='highlight').text)
+    odds.append(teamLines.find_next('a', class_='highlight').text.strip().split()[0])
 odds_dict_f5 = {teams[i]: odds[i:i + 2] for i in range(0,len(teams),2)}
 
 print(odds_dict_f5)
