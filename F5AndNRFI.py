68aa61e54f8303cbb6f03197f90dd819d03b569c
bena618 <98246992+bena618@users.noreply.github.com>
2025-04-08 13:13:04 -0400

    Forgot to get text from the html

--- F5AndNRFI.py
+++ F5AndNRFI.py
@@ -601,7 +601,7 @@ allTeamLines = allTeamLines[1:]
 
 odds = []
 for teamLines in allTeamLines:
-    odds.append(teamLines.find_next('a', class_='highlight'))
+    odds.append(teamLines.find_next('a', class_='highlight').text.strip())
 odds_dict_f5 = {teams[i]: odds[2 * i: 2 * i + 2] for i in range(0,len(teams),2)}
 
 print(odds_dict_f5)
