638e383ff940ba9be2c99efd719aa0a96e14d932
bena618 <98246992+bena618@users.noreply.github.com>
2025-04-14 16:24:26 -0400

    Update Hits.py

--- Hits.py
+++ Hits.py
@@ -160,7 +160,7 @@ players_hit_lines = {}
 base_url = "https://api.bettingpros.com/"
 
 player_points_dict = {}
-endpoint = "v3/props?limit=50&page=1&sport=MLB&market_id=285&date={datetime.strptime(todaysDate, "%m/%d/%Y").strftime("%Y-%m-%d")}&location=MD&book_id=0:10:12:19:33:13:18&sort=diff&sort_direction=desc&performance_type_sort=last_15&include_selections=false&include_markets=true&min_odds=-1000&max_odds=1000&ev_threshold_min=-0.4&ev_threshold_max=0.4&performance_type_filter=last_15"
+endpoint = f"v3/props?limit=50&page=1&sport=MLB&market_id=285&date={datetime.strptime(todaysDate, "%m/%d/%Y").strftime("%Y-%m-%d")}&location=MD&book_id=0:10:12:19:33:13:18&sort=diff&sort_direction=desc&performance_type_sort=last_15&include_selections=false&include_markets=true&min_odds=-1000&max_odds=1000&ev_threshold_min=-0.4&ev_threshold_max=0.4&performance_type_filter=last_15"
 
 while endpoint is not None:
     url = f'{base_url}{endpoint}'
@@ -196,49 +196,7 @@ while endpoint is not None:
         
         player_points_dict[player_name] = [line, best_cost_over, best_cost_under]
     endpoint = json_data.get("_pagination", {}).get("next")    
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-url = f'https://api.bettingpros.com/'
-
-response = requests.get(url,headers=headers)
-
-if response.status_code != 200:
-    print(f"Failed to fetch data: {response.status_code}")
-
-json_data = response.json()
-
-for prop in json_data.get("props", []):
-    player_name = prop.get("participant", {}).get("name")
-    over = prop.get("over", {})
-    line = over.get("line")
-    over_odds = over.get("odds")
-    under_odds = prop.get("under", {}).get("odds")
-
-    if over_odds is None or under_odds is None:
-        continue
-
-    players_hit_lines[player_name] = [
-        line,
-        over_odds,
-        under_odds
-    ]
-
+    
 #players_hit_lines[f"{player_name} @ {game_times[indices[0]//18]}"] = line,odds
 
 # %%
