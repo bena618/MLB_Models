c3bd19e1ebdcad6a34bc046f2a9f14294d80e0d1
bena618 <98246992+bena618@users.noreply.github.com>
2025-04-24 01:31:05 -0400

    Update Ks.py

--- Ks.py
+++ Ks.py
@@ -127,7 +127,7 @@ if response.status_code == 200:
 
     teams = soup.find_all('div',class_= 'lineup__abbr')
     teams = [elem.text for elem in teams]
-#    teams[6:] = teams[8:]
+    teams[8:] = teams[10:]
 
     game_times = soup.find_all('div',class_="lineup__time")
     game_times = [elem.text for elem in game_times]
