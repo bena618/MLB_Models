4ad3bdda743046a7b6c6e8e2cb446566bca3c167
bena618 <98246992+bena618@users.noreply.github.com>
2025-04-30 13:28:28 -0400

    Update F5AndNRFI.py

--- F5AndNRFI.py
+++ F5AndNRFI.py
@@ -67,7 +67,7 @@ def get_pitcher_data(name):
     if name == 'E. Rodriguez':
         name = 'Eduardo Rodriguez'
 
-#    print(f"{name}:{url}")
+    print(f"{name}:{url}")
     response = requests.get(url, headers=headers)
     if response.status_code == 200:
         stats = response.json()
@@ -84,7 +84,7 @@ def get_pitcher_data(name):
 #            url = 'https://www.statmuse.com/mlb/ask/' + name.lower().replace(' ', '-') + '-stats-last-10-games-including-whip'
             url = 'https://www.statmuse.com/mlb/ask/' + name.lower().replace(' ', '-') + '-stats-last-10-games-including-whip-log'
             response = requests.get(url, headers=headers)
-#            print(f"In except-P: {url}")
+            print(f"In except-P {name}: {url}")
             try:
                 tables = pd.read_html(response.text)
                 df = tables[0].head(10)
@@ -96,7 +96,7 @@ def get_pitcher_data(name):
 #                print("Pitcher DF: ",df) 
                 return {"Name": name,"whip": whip_L10}
             except Exception as e:
-                print(f"help-p: {url}")
+                print(f"help-p {name}: {url}")
                 print(response.text)
                 print(e)
 #                raise
@@ -294,7 +294,7 @@ if response.status_code == 200:
    # Manual adjustments are sometimes needed because sometimes in baseball there is a different pitcher for just the first inning as oppose to the main pitcher 
    # and for the part of the code that makes predictions for the 1st 5 innings main pitcher more important
    pitchers_for_1st = pitchers[:] 
-   pitchers_for_1st[17] = {"Name": 'Tyler Gilbert',"whip": 1.41}
+#   pitchers_for_1st[17] = {"Name": 'Tyler Gilbert',"whip": 1.41}
 #   pitchers_for_1st[6]['whip'] = .6
     
    batters = soup.find_all('li',class_ = 'lineup__player')
