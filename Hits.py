594fa394081f4be95d25356b0dd6f107cb44721d
bena618 <98246992+bena618@users.noreply.github.com>
2025-05-03 21:44:43 -0400

    Update Hits.py

--- Hits.py
+++ Hits.py
@@ -128,11 +128,11 @@ if response.status_code == 200:
 
     teams = soup.find_all('div',class_= 'lineup__abbr')
     teams = [elem.text for elem in teams]
-    teams[4:] = teams[6:]
+#    teams[4:] = teams[6:]
     
     game_times = soup.find_all('div',class_="lineup__time")[:-2]
     game_times = [elem.text for elem in game_times]
-    game_times[2:] = game_times[3:]
+#    game_times[2:] = game_times[3:]
 
     pitchers_stats = {}
     for i,pitcher in enumerate(pitchers):
