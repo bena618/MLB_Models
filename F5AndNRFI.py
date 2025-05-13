fc0b845116487a82cebc9c78bd43b0e0e1bff4dd
bena618 <98246992+bena618@users.noreply.github.com>
2025-05-13 15:57:29 -0400

    Update F5AndNRFI.py

--- F5AndNRFI.py
+++ F5AndNRFI.py
@@ -308,13 +308,12 @@ if response.status_code == 200:
    teams = [elem.text for elem in teams]
 #   teams[26:] = teams[28:] 
 
-#   teams[6:] = teams[8:] this as well as game_time if there is a game that is marked as not happening ie.postponed then website still shows it so have to do this to adjust
    teams[2:] = teams[4:]
-#   teams[24:] = teams[26:]    
+   teams[6:] = teams[8:]    
    game_times = soup.find_all('div',class_="lineup__time")
    game_times = [elem.text for elem in game_times][:-2]
    game_times[1:] = game_times[2:]
-#   game_times = game_times[1:] 
+   game_times[3:] = game_times[4:] 
 #   game_times[:] = game_times[1:]
 #   game_times[12:] = game_times[13:]    
 
