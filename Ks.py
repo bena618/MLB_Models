ec778c560e39e64f33612a8728300428802e020e
bena618 <98246992+bena618@users.noreply.github.com>
2025-05-14 11:15:05 -0400

    Update Ks.py

--- Ks.py
+++ Ks.py
@@ -127,13 +127,13 @@ if response.status_code == 200:
 
     teams = soup.find_all('div',class_= 'lineup__abbr')
     teams = [elem.text for elem in teams]
-    teams[2:] = teams[4:]
-    teams[6:] = teams[8:]
+#    teams[2:] = teams[4:]
+#    teams[6:] = teams[8:]
 
     game_times = soup.find_all('div',class_="lineup__time")
     game_times = [elem.text for elem in game_times]
-    game_times[1:] = game_times[2:]
-    game_times[3:] = game_times[4:]
+#    game_times[1:] = game_times[2:]
+#    game_times[3:] = game_times[4:]
 
     team_strikeout_rate = {}
     team_so_rate = 0
