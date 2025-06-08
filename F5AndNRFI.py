230fd5d660a8b534b5fea0737faf38d4cbd4edc1
bena618 <98246992+bena618@users.noreply.github.com>
2025-06-08 11:55:14 -0400

    Update F5AndNRFI.py

--- F5AndNRFI.py
+++ F5AndNRFI.py
@@ -296,8 +296,8 @@ if response.status_code == 200:
    # Manual adjustments are sometimes needed because sometimes in baseball there is a different pitcher for just the first inning as oppose to the main pitcher 
    # and for the part of the code that makes predictions for the 1st 5 innings main pitcher more important
    pitchers_for_1st = pitchers[:] 
-   pitchers_for_1st[1] = {"name": 'Tyler Holton', 'whip':1.5}
-   # pitchers_for_1st[19] = {"name": 'Jared Shuster', 'whip':1.3}
+   pitchers_for_1st[0] = {"name": 'Anthony Veneziano', 'whip':1.2}
+   pitchers_for_1st[4] = {"name": 'Jacob Latz', 'whip':1.17}
    # pitchers_for_1st[-3] = {"name": 'Justin Sterner', 'whip':2.3}
     
    batters = soup.find_all('li',class_ = 'lineup__player')
