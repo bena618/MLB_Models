95bc007ac64ab26712b8f27bd3d699dfe4daeec1
bena618 <98246992+bena618@users.noreply.github.com>
2025-07-21 18:25:25 -0400

    Update F5AndNRFI.py

--- F5AndNRFI.py
+++ F5AndNRFI.py
@@ -377,10 +377,10 @@ def simulate_at_bat(batter_avg,single_prob,double_prob,triple_prob,hr_prob, pitc
         out_prob = .25
         print("force")         
 
-    # try:
-    outcome = np.random.choice(['hit', 'walk', 'out'], p=[hit_prob, walk_prob, out_prob])
-    # except:
-    #     outcome = 'out'#Fix later
+     try:
+        outcome = np.random.choice(['hit', 'walk', 'out'], p=[hit_prob, walk_prob, out_prob])
+     except:
+        outcome = 'out'#Fix later
 
     if outcome == 'hit':
         outcome = np.random.choice(['single', 'double', 'triple','home_run'], p=[single_prob, double_prob, triple_prob,hr_prob])
