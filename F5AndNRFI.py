16d41090ca639587e657dd9ed090dac0339db0e7
bena618 <98246992+bena618@users.noreply.github.com>
2026-04-16 12:32:29 -0400

    Update F5AndNRFI.py

--- F5AndNRFI.py
+++ F5AndNRFI.py
@@ -170,7 +170,7 @@ if response.status_code == 200:
    # and for the part of the code that makes predictions for the 1st 5 innings main pitcher more important
    pitchers_for_1st = pitchers[:] 
    pitchers_for_1st[11] = {"name": 'Jordan Leasure' ,"whip": 1}
-   pitchers_for_1st[18] = {"name": 'Juan Mejia' ,"whip": 1.8}
+   pitchers_for_1st[16] = {"name": 'Juan Mejia' ,"whip": 1.8}
    # pitchers_for_1st[17] = {"name": 'Shohei Ohtani' ,"whip": 1.35}
 
    batters = soup.find_all('li',class_ = 'lineup__player')
