74268fd1516e7c872e23e8a2fef0a407ff1434b8
bena618 <98246992+bena618@users.noreply.github.com>
2026-05-05 14:29:52 -0400

    Update F5AndNRFI.py

--- F5AndNRFI.py
+++ F5AndNRFI.py
@@ -185,11 +185,11 @@ if response.status_code == 200:
 
    teams = soup.find_all('div',class_= 'lineup__abbr')
    teams = [elem.text for elem in teams]
-   #teams[16:] = teams[18:] 
+   teams[20:] = teams[22:] 
 
    game_times = soup.find_all('div',class_="lineup__time")
    game_times = [elem.text for elem in game_times][:-1]
-   #game_times[8:] = game_times[9:]
+   game_times[10:] = game_times[11:]
 
    print(game_times)
 
