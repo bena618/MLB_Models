d4f91af7f175345a42f8d878f6ad26ce16bbb309
bena618 <98246992+bena618@users.noreply.github.com>
2026-05-09 15:29:14 -0400

    Update F5AndNRFI.py

--- F5AndNRFI.py
+++ F5AndNRFI.py
@@ -169,8 +169,8 @@ if response.status_code == 200:
    # Manual adjustments are sometimes needed because sometimes in baseball there is a different pitcher for just the first inning as oppose to the main pitcher 
    # and for the part of the code that makes predictions for the 1st 5 innings main pitcher more important
    pitchers_for_1st = pitchers[:] 
-   # pitchers_for_1st[0] = {"name": 'Jovani Moran' ,"whip": 1.2}
-   #pitchers_for_1st[3] = {"name": 'Tyler Holton' ,"whip": 2}
+   pitchers_for_1st[8] = {"name": 'Richard Lovelady' ,"whip": 1.7}
+   pitchers_for_1st[18] = {"name": 'Burch Smith' ,"whip": 1.2}
    #pitchers_for_1st[17] = {"name": 'Steven Okert',"whip": 1.5}
    # pitchers_for_1st[17] = {"name": 'Shohei Ohtani' ,"whip": 1.35}
 
